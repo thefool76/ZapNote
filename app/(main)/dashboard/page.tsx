359c369d8cefdaa12890949b4417dcf003152dc3
sumit <sumitmishra1135@gmail.com>
2024-02-15 17:12:45 +0530

    done with the whole setup minor errors solved

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+import db from "@/lib/supbase/db";
+import Dashboardsetup from "@/components/Dashboard-setup";
 
-const Page = () => {
-  return <div>Page</div>
+const DashbaordPage = async () => {
+  const supabase = createServerComponentClient({cookies});
+  const {data:{user}, 
+  } = await supabase.auth.getUser();
+
+  if(!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace,{eq}) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  if (!workspace)
+  return <div className="bg-background h-screen w-screen flex justify-center items-center">
+    
+  </div>
+
+  redirect(`/dashboard/${workspace.id}`);
+  
 }
 
-export default Page
+export default DashbaordPage
---
+++ components/Dashboard-setup.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { AuthUser } from '@supabase/supabase-js';
+import { Card,CardContent,CardDescription,CardHeader, CardTitle } from './ui/card';
+
+  interface DashboardSetupProps{
+    user:AuthUser;
+    subscription: {} | null;
+  }
+
+const Dashboardsetup: React.FC<DashboardSetupProps> = ({
+  subscription,
+  user,
+}) => {
+  return (
+    <Card className='w-[800px] h-screen sm:h-auto'>
+      <CardHeader>
+        <CardTitle>
+          Create A Workspace
+        </CardTitle>
+        <CardDescription>
+          Setting up your workspace!. You can add collaborators later on from the workspace setting tab.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={() => {}}>
+          <div className='flex flex-col gap-4'></div>
+
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Dashboardsetup
